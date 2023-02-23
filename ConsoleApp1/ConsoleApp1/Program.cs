26dc47f5c47c94d65bb37faed660b52be8345f42
iradaglly <kerimovainci001@gmail.com>
2023-02-23 12:53:56 +0400

    1st commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skafx = 1, skafy = 4 , skafz=5.5;
+            double doorx = 4, doory = 4;
+
+            if (((doorx >= skafx) && (doory >= skafy)))
+            {
+                Console.WriteLine("kece biler.");
+            }
+            else if ((doorx >= skafz) && (doory >= skafx))
+            {
+                Console.WriteLine("kece biler.");
+            }
+            else if ((doorx >= skafy) && (doory >= skafz))
+            {
+                Console.WriteLine("kece biler.");
+            }
+            else if (((doorx >= skafy) && (doory >= skafz)) || ((doorx >= skafx) && (doory >= skafy)))
+            {
+                Console.WriteLine("kece biler.");
+            }
+            else if (((doorx >= skafy) && (doory >= skafz)) || ((doorx >= skafz) && (doory >= skafx)))
+            {
+                Console.WriteLine("kece biler.");
+            }
+            else
+            {
+                Console.WriteLine("Kece bilmez!");
+            }
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,39 @@
+//arithmetic operators
+let x=15;
+let y=7;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x**y);
+console.log(x/y);
+console.log(x%y);
+console.log(++x);
+console.log(x++);
+console.log(x++);
+console.log(--y);
+console.log(y--);
+// assignment operators
+console.log(x=y);
+console.log(x+=y);
+console.log(x-=y);
+console.log(x*=y);
+console.log(x/=y);
+console.log(x%=y);
+console.log(x**=y);
+//strings
+let text1="hello"
+let text2="world"
+console.log(text1+" "+text2);
+//comparison operators
+let a=6
+let b=8
+console.log(x==y);
+console.log(x===y);
+console.log(x!=y);
+console.log(x!==y);
+console.log(x>y);
+console.log(x<y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);=
+
